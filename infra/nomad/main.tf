653c95213483958cc625a0019286e401966e53a4
Thomas Obenaus <thomas.obenaus@preh.de>
2018-03-19 20:57:11 +0100

    .UPDATE formatting + clean up

--- infra/nomad/main.tf
+++ infra/nomad/main.tf
@@ -1,17 +1,3 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY A NOMAD CLUSTER AND A SEPARATE CONSUL CLUSTER IN AWS
-# These templates show an example of how to use the nomad-cluster module to deploy a Nomad cluster in AWS. This cluster
-# connects to Consul running in a separate cluster.
-#
-# We deploy two Auto Scaling Groups (ASGs) for Nomad: one with a small number of Nomad server nodes and one with a
-# larger number of Nomad client nodes. Note that these templates assume that the AMI you provide via the
-# nomad_ami_id input variable is built from the examples/nomad-consul-ami/nomad-consul.json Packer template.
-#
-# We also deploy one ASG for Consul which has a small number of Consul server nodes. Note that these templates assume
-# that the AMI you provide via the consul_ami_id input variable is built from the examples/consul-ami/consul.json
-# Packer template in the Consul AWS Module.
-# ---------------------------------------------------------------------------------------------------------------------
-
 provider "aws" {
   profile = "${var.aws_profile}"
   region  = "${var.aws_region}"
@@ -24,51 +10,10 @@ terraform {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# AUTOMATICALLY LOOK UP THE LATEST PRE-BUILT AMI
-# This repo contains a CircleCI job that automatically builds and publishes the latest AMI by building the Packer
-# template at /examples/nomad-consul-ami upon every new release. The Terraform data source below automatically looks up
-# the latest AMI so that a simple "terraform apply" will just work without the user needing to manually build an AMI and
-# fill in the right value.
-#
-# !! WARNING !! These exmaple AMIs are meant only convenience when initially testing this repo. Do NOT use these example
-# AMIs in a production setting because it is important that you consciously think through the configuration you want
-# in your own production AMI.
-#
-# NOTE: This Terraform data source must return at least one AMI result or the entire template will fail. See
-# /_ci/publish-amis-in-new-account.md for more information.
-# ---------------------------------------------------------------------------------------------------------------------
-data "aws_ami" "nomad_consul" {
-  most_recent      = true
-
-  # If we change the AWS Account in which test are run, update this value.
-  owners     = ["562637147889"]
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "is-public"
-    values = ["true"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["nomad-consul-ubuntu-*"]
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY THE NOMAD SERVER NODES
 # ---------------------------------------------------------------------------------------------------------------------
-
 module "nomad_servers" {
-  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-  # to a specific version of the modules, such as the following example:
   source = "git::git@github.com:ThomasObenaus/terraform-aws-nomad.git//modules/nomad-cluster?ref=master"
-  #source = "git::git@github.com:hashicorp/terraform-aws-nomad.git//modules/nomad-cluster?ref=v0.3.0"
-  #source = "../../modules/nomad-cluster"
 
   cluster_name  = "${var.nomad_cluster_name}-server"
   instance_type = "t2.micro"
@@ -78,7 +23,7 @@ module "nomad_servers" {
   max_size         = "${var.num_nomad_servers}"
   desired_capacity = "${var.num_nomad_servers}"
 
-  ami_id    = "${var.ami_id == "" ? data.aws_ami.nomad_consul.image_id : var.ami_id}"
+  ami_id    = "${var.ami_id}"
   user_data = "${data.template_file.user_data_nomad_server.rendered}"
 
   vpc_id     = "${data.aws_vpc.default.id}"
@@ -86,7 +31,8 @@ module "nomad_servers" {
 
   # To make testing easier, we allow requests from any IP address here but in a production deployment, we strongly
   # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
-  allowed_ssh_cidr_blocks     = ["0.0.0.0/0"]
+  allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
+
   allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
   ssh_key_name                = "${var.ssh_key_name}"
 }
@@ -96,10 +42,8 @@ module "nomad_servers" {
 # To allow our server Nodes to automatically discover the Consul servers, we need to give them the IAM permissions from
 # the Consul AWS Module's consul-iam-policies module.
 # ---------------------------------------------------------------------------------------------------------------------
-
 module "consul_iam_policies_servers" {
-  source = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-iam-policies?ref=v0.1.0"
-
+  source      = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-iam-policies?ref=v0.1.0"
   iam_role_id = "${module.nomad_servers.iam_role_id}"
 }
 
@@ -107,7 +51,6 @@ module "consul_iam_policies_servers" {
 # THE USER DATA SCRIPT THAT WILL RUN ON EACH NOMAD SERVER NODE WHEN IT'S BOOTING
 # This script will configure and start Nomad
 # ---------------------------------------------------------------------------------------------------------------------
-
 data "template_file" "user_data_nomad_server" {
   template = "${file("${path.module}/user-data-nomad-server.sh")}"
 
@@ -121,7 +64,6 @@ data "template_file" "user_data_nomad_server" {
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY THE CONSUL SERVER NODES
 # ---------------------------------------------------------------------------------------------------------------------
-
 module "consul_servers" {
   source = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-cluster?ref=v0.1.0"
 
@@ -133,7 +75,7 @@ module "consul_servers" {
   cluster_tag_key   = "${var.cluster_tag_key}"
   cluster_tag_value = "${var.consul_cluster_name}"
 
-  ami_id    = "${var.ami_id == "" ? data.aws_ami.nomad_consul.image_id : var.ami_id}"
+  ami_id    = "${var.ami_id}"
   user_data = "${data.template_file.user_data_consul_server.rendered}"
 
   vpc_id     = "${data.aws_vpc.default.id}"
@@ -141,7 +83,8 @@ module "consul_servers" {
 
   # To make testing easier, we allow Consul and SSH requests from any IP address here but in a production
   # deployment, we strongly recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
-  allowed_ssh_cidr_blocks     = ["0.0.0.0/0"]
+  allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
+
   allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
   ssh_key_name                = "${var.ssh_key_name}"
 }
@@ -150,7 +93,6 @@ module "consul_servers" {
 # THE USER DATA SCRIPT THAT WILL RUN ON EACH CONSUL SERVER EC2 INSTANCE WHEN IT'S BOOTING
 # This script will configure and start Consul
 # ---------------------------------------------------------------------------------------------------------------------
-
 data "template_file" "user_data_consul_server" {
   template = "${file("${path.module}/user-data-consul-server.sh")}"
 
@@ -163,32 +105,27 @@ data "template_file" "user_data_consul_server" {
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY THE NOMAD CLIENT NODES
 # ---------------------------------------------------------------------------------------------------------------------
-
 module "nomad_clients" {
-  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-  # to a specific version of the modules, such as the following example:
-  #source = "git::git@github.com:hashicorp/terraform-aws-nomad.git//modules/nomad-cluster?ref=v0.3.0"
   source = "git::git@github.com:ThomasObenaus/terraform-aws-nomad.git//modules/nomad-cluster?ref=master"
-  #source = "../../modules/nomad-cluster"
 
   cluster_name  = "${var.nomad_cluster_name}-client"
   instance_type = "t2.micro"
 
   # To keep the example simple, we are using a fixed-size cluster. In real-world usage, you could use auto scaling
   # policies to dynamically resize the cluster in response to load.
-  min_size         = "${var.num_nomad_clients}"
+  min_size = "${var.num_nomad_clients}"
+
   max_size         = "${var.num_nomad_clients}"
   desired_capacity = "${var.num_nomad_clients}"
-
-  ami_id    = "${var.ami_id == "" ? data.aws_ami.nomad_consul.image_id : var.ami_id}"
-  user_data = "${data.template_file.user_data_nomad_client.rendered}"
-
-  vpc_id     = "${data.aws_vpc.default.id}"
-  subnet_ids = "${data.aws_subnet_ids.default.ids}"
+  ami_id           = "${var.ami_id}"
+  user_data        = "${data.template_file.user_data_nomad_client.rendered}"
+  vpc_id           = "${data.aws_vpc.default.id}"
+  subnet_ids       = "${data.aws_subnet_ids.default.ids}"
 
   # To make testing easier, we allow Consul and SSH requests from any IP address here but in a production
   # deployment, we strongly recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
-  allowed_ssh_cidr_blocks     = ["0.0.0.0/0"]
+  allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
+
   allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
   ssh_key_name                = "${var.ssh_key_name}"
 }
